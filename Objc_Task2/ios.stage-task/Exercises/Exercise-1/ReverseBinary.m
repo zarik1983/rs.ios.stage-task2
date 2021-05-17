9637c52ee7241352bb8136fb94ccfaf16fc47d5c
Aliaksandr Zarudni <azarudniy@yandex.ru>
2021-05-17 20:30:33 +0300

    feat: 2 examples solution

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,25 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    NSString *stringStraightBin = @"";
+    NSString *tempString = @"";
+    NSMutableString *reversedString = [NSMutableString string];
+    NSInteger x = n;
+    
+    while (x>0) {
+        stringStraightBin = [[NSString stringWithFormat:@"%lu", x&1] stringByAppendingString:stringStraightBin];
+        x = x>>1;
+    }
+    for (int i=0; i<8-[stringStraightBin length]; i++) {
+        tempString = [tempString stringByAppendingString:@"0"];
+    }
+    stringStraightBin = [tempString stringByAppendingString:stringStraightBin];
+    NSInteger charIndex = [stringStraightBin length];
+    while (charIndex>0) {
+        charIndex--;
+        NSRange subRange = NSMakeRange(charIndex, 1);
+        [reversedString appendString:[stringStraightBin substringWithRange:subRange]];
+    }
+    
+    return strtol([reversedString UTF8String], NULL, 2);
 }
--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,15 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    NSInteger count = 0;
+    for (int i=0; i<[array count]-1; i++) {
+        for (int j=1; j<[array count]; j++) {
+            if ([array[j] intValue] - [array[i] intValue] == [number intValue]) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
 
 @end
