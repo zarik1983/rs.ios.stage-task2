9093b8ceff51233209715a7f042ebaf4412efda8
Aliaksandr Zarudni <azarudniy@yandex.ru>
2021-05-17 23:39:47 +0300

    feat: part 4th example solution

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -26,24 +26,6 @@
     outputString = [outputString substringToIndex:[outputString length]-1];
     return outputString;
     
-    /*for (int i=0; i<[numbersArray count]; i++) {
-        if ([numbersArray[i] intValue] < 0) {
-            return @"Negative numbers are not valid for input.";
-        }
-        if ([numbersArray[i] intValue] > 255) {
-            return @"The numbers in the input array can be in the range from 0 to 255.";
-        }
-        outputString = [outputString stringByAppendingString: [numbersArray[i] stringValue]];
-        outputString = [outputString stringByAppendingString:@"."];
-    }
-    for (int i=0; i<4-[numbersArray count]; i++) {
-        outputString = [outputString stringByAppendingString:@"0."];
-    }
-    outputString = [outputString substringToIndex:[outputString length]-1];
-    if ([numbersArray count] > 0 && numbersArray != nil) {
-        return outputString;
-    }*/
-    //return @"";
 }
 
 @end
--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,36 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    NSMutableArray *numbers = [NSMutableArray array];
+    NSMutableArray *strings = [NSMutableArray array];
+    
+    if ([array count] < 2) {
+        NSString *className = NSStringFromClass([array[0][0] class]);
+        for (int i=1; i<[array[0] count]; i++) {
+            BOOL flag = (![className isEqual: NSStringFromClass([array[0][i] class])]);
+            if (flag) {
+                return @[];
+            }
+        }
+    }
+    
+    for (int i=0; i<[array count]; i++) {
+        for (int j=0; j<[array[i] count]; j++) {
+            if ([NSStringFromClass([array[i][j] class])  isEqual: @"__NSCFNumber"]) {
+                [numbers addObject: array[i][j]];
+            } else {
+                [strings addObject: array[i][j]];
+            }
+        }
+    }
+    numbers = [NSMutableArray arrayWithArray:[numbers sortedArrayUsingSelector:@selector(compare:)]];
+        
+    return numbers;
+    
+    
+    
+    
+    //return @[];
 }
 
 @end
